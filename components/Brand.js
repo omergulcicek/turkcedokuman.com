db28f91bb5227630be7e033f82e8357bb2bfbce2
Ömer Gülçiçek <iletisim@omergulcicek.com>
2023-09-02 01:48:48 +0300

    Klasör dizini değişti

--- components/Brand.js
+++ components/Brand.js
@@ -1,7 +1,7 @@
 export default function Brand() {
   return (
     <>
-      <h1 className="">
+      <h1 className="cursor-pointer select-none">
         <span className="text-neutral-900 text-2xl font-semibold">
           turkcedokuman
         </span>
