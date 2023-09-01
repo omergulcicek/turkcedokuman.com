6718c8f2caa815b91e7d50aa49d80a2805c01b46
Ömer Gülçiçek <iletisim@omergulcicek.com>
2023-09-02 01:04:20 +0300

    dark theme

--- v2/components/AddCard.js
+++ v2/components/AddCard.js
@@ -5,7 +5,7 @@ export default function AddCard() {
   return (
     <>
       <article className="m-[10px] h-40 w-80 select-none p-[1px] dark:border-neutral-800 rounded-lg border border-dashed">
-        <div className="bg-white dark:from-neutral-800 dark:to-neutral-900 relative flex h-full flex-col justify-between p-4 transition">
+        <div className="bg-gradient-to-t dark:from-neutral-800 dark:to-neutral-900 hover:bg-white relative flex h-full flex-col justify-between p-4 transition">
           <Link
             href="https://github.com/omergulcicek/turkcedokuman.com"
             target="_blank"
